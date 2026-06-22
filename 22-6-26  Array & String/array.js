517f1987596749b604734ffbd819b9eef663d7d6
rokadrajal <rokadrajal@gmail.com>
2026-06-22 12:19:15 +0530

    array/string

---
+++ 22-6-26  Array & String/array.js
@@ -0,0 +1,67 @@
+// Array : Array is collection of element which same datatype / different datatype
+// index : start 0
+// syntax : []
+
+var arr = [];
+console.log(arr);
+
+var arr = [10 , 11 , 12];
+console.log(arr);
+
+var arr = ["sita","ram"];
+console.log(arr);
+
+var arr = [10 , 20 ,"sita","ram",true , undefined , null];
+console.log(arr);
+
+var arr = [10 , 11 , 12 , 15 , 18];
+console.log(arr.length);
+console.log(arr[2]);
+
+
+// 1 . push
+// add a new element in last index
+// return : new length
+
+arr.push(100);
+console.log(arr);
+
+var data = arr.push(200);
+console.log(data);  
+
+
+// 2 . pop
+// remove element in last index
+// return : remove element
+
+arr.pop();
+console.log(arr);
+
+var data2 = arr.pop();
+console.log(data2);
+
+
+
+// 3 . unshift
+// add a new Element  : 0 index
+// return : new length
+
+arr.unshift(100,true);
+console.log(arr);
+
+
+// 4. shift
+// remove element in 0 index
+// return : remove element
+
+arr.shift();
+console.log(arr);
+
+
+// 5 . splice
+// arr.splice(0); // array empty thay jay
+// console.log(arr);
+
+arr.splice(0,2); // 0-index , 2 number of element
+console.log(arr);
+
---
+++ 22-6-26  Array & String/string.js
@@ -0,0 +1,61 @@
+// String : It's a Collection of Chars.
+
+// // using Double Quote : 
+var fname = "Ram";
+console.log(fname,typeof fname);
+
+// // using Single Quote : 
+var alpha = 'abcd';
+console.log(alpha,typeof alpha);
+
+// // using Backtick :   string formating  multiline string
+var lname = `Shah`;
+console.log(lname,typeof lname);
+
+
+var fname = "ram";
+var age = 18;
+
+console.log("Your name is ",fname,".","Your age is ",age,".");
+console.log('Your name is ',fname,'.','Your age is ',age,'.');
+
+
+// string formating
+
+console.log(`Your name is ${fname} . Your age is ${age}`);
+console.log("Your name is ${fname} . Your age is ${age}"); // wrong
+console.log('Your name is ${fname} . Your age is ${age}'); // wrong
+
+
+// console.log("this  // error
+//      is java");
+
+// console.log('this  // error
+//      is java');
+
+
+// multiline string
+console.log(`this 
+is java`);
+
+console.log(`this 
+    is java`);
+
+var str = "radha krishna";
+console.log(str);
+
+//1 . length : property
+console.log(str.length);
+
+// 2. method
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str.includes("ew"));
+
+console.log(str.split());
+console.log(str.split("")); // latter spaccing
+console.log(str.split(" "));// word spaccing
+
+var str2 = "java";
+console.log(str2[2]);
