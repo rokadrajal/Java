5d69bb209e02cf5ade567101c3bb1aaec6590027
rokadrajal <rokadrajal@gmail.com>
2026-06-25 15:50:23 +0530

    object and looping

---
+++ 25-6-26 object and looping/loop.js
@@ -0,0 +1,135 @@
+var user = {
+    fname : "ram",
+    lname : "patel",
+    age : 18,
+    gender : "male",
+    contactNo : 1234567890,
+}
+console.log(user);
+
+//basic loop = array , for of = array , for in = array & object
+// 1. object loop
+
+for (const key in user) {
+   console.log(key,user[key]);
+}
+
+
+// 2. computing object : []
+var key1 = "fname";
+var key2 = "lname";
+
+var value1 = "radhe";
+var value2 = "patel";
+
+var obj1 = {  
+    key1 : value1,
+    key2 : value2,
+}
+console.log(obj1); // OUTPUT = key1 : radhe , key2 : patel
+
+
+var obj2 = {
+    [key1] : value1,
+    [key2] : value2,
+}
+
+console.log(obj2); // OUTPUT = fname : radhe , lname : patel
+
+
+
+// 3 . Nested object
+
+var data = {
+    fname : "ram",
+    lname : "patel",
+    age : 18,
+    gender : "male",
+    contactNo : 1234567890,
+
+    address : {
+        city : "rajkot",
+        state : "Gujarat",
+        nation : "India",
+    }
+
+}
+
+console.log(data);
+
+console.log(data.address);
+console.log(data["address"]);
+
+console.log(data.address.city);
+console.log(data["address"].city);
+console.log(data.address["city"]);
+console.log(data["address"]["city"]);
+
+
+
+var data1 = {
+    fname : "ram",
+    lname : "patel",
+    age : 18,
+    gender : "male",
+    contactNo : 1234567890,
+}
+
+console.log(data1);
+
+console.log(data1.address); // address is not available aetle undefind
+console.log(data1["address"]);
+
+// console.log(data1.address.city); // address is not available aetle undefind aetle city is not possiable aetle error
+// console.log(data1["address"].city);
+// console.log(data1.address["city"]);
+// console.log(data1["address"]["city"]);
+
+
+
+// 4.optional chaining :   Nested object error solve mate
+// syntex : (?.)
+
+
+console.log(data1?.address?.city); // address is not available aetle undefind pacchi te city pase jayj nay tyathij exit
+console.log(data1?.["address"]?.city);
+console.log(data1?.address?.["city"]);
+console.log(data1?.["address"]?.["city"]);
+
+
+// 5.Array in object
+
+var product = {
+    fname : "ram",
+    lname : "patel",
+    age : 18,
+    gender : "male",
+    contactNo : 1234567890,
+    hobbies : ["design" , "play"],
+
+}
+console.log(product);
+console.log(product.hobbies);
+console.log(product.hobbies[0]);
+
+
+// 6. object in array
+
+var arr = [
+    {"id":1 , "fname":"sita" , "weight":40},
+    {"id":2 , "fname":"Ram" , "weight":50},
+    {"id":3 , "fname":"hanuman" , "weight":60}
+]
+
+console.log(arr);
+console.log(arr[1]);
+
+
+
+
+
+
+
+
+
+
