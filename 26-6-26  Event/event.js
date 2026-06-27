cc21ee221dcc6d7ce86e67902c69d9c42f746f66
rokadrajal <rokadrajal@gmail.com>
2026-06-27 08:27:32 +0530

    Event

---
+++ 26-6-26  Event/event.js
@@ -0,0 +1,27 @@
+// function printData()
+// {
+//     console.log("welcome to js");
+// }
+
+
+document.querySelector('button').onclick = function () {
+    console.log("welcome to js");
+    console.log("This is second line");
+}
+
+
+document.querySelector('button').onclick = function (){
+    // how to chech class list
+    // console.log(document.querySelector('h1').classList);
+
+    // how to add new class
+    // document.querySelector('h1').classList.add("show");
+
+    // how to remove class
+    // document.querySelector('h1').classList.remove("show");
+
+
+    // how to add Toggle : add + remove
+    document.querySelector('h1').classList.toggle('show');
+
+}
